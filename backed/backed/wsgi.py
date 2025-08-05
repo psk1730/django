437a14e6b2e2593db10f09e6a17d52756447e7d1
Pravesh Kumar <pravesh.psk@gmail.com>
2025-08-05 12:34:33 +0530

    Fix sys.path in wsgi to include root for core import

--- backed/backed/wsgi.py
+++ backed/backed/wsgi.py
@@ -1,16 +1,29 @@
-"""
-WSGI config for backed project.
+# """
+# WSGI config for backed project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+# """
+
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backed.settings')
+
+# application = get_wsgi_application()
 
 import os
+import sys
+
+# ✅ Add this line to include the outer 'backed/' directory in sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backed.settings')
 
 application = get_wsgi_application()
+
