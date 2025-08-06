ee711f5e5a73d6c4622694f1885cff6e7a6aedc9
Pravesh Kumar <pravesh.psk@gmail.com>
2025-08-06 17:30:31 +0530

    Fix sys.path in wsgi to include root for core import

--- backed/backed/settings.py
+++ backed/backed/settings.py
@@ -52,8 +52,11 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,6 +135,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
